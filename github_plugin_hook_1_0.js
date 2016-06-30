2c6afb7780066527036490a04888aa71eda70b3f
timhiggins3118 <tim.higgins@live.com>
2016-06-30 07:39:10 -0400

    Add files via upload

--- github_plugin_hook_1_0.js
+++ github_plugin_hook_1_0.js
@@ -1,4 +1,4 @@
-module['exports'] = function changeGitHubIssueState (hook) {
+module['exports'] = function changeGitHubIssueState(hook) {
     // Read task inputs
     var request = require('request'),
         endPointProperties = hook.req.body.endPointProperties,
@@ -11,26 +11,26 @@ module['exports'] = function changeGitHubIssueState (hook) {
         newStatus = taskProperties.issueStatus;
 
     authorization = authorization == "Trust me" ? hook.env.githubAuth : authorization;
-    headers = {'Authorization': authorization, 'User-Agent': 'request'};
-    requestBody = JSON.stringify({ "state" : newStatus });
-    console.log("Tim - user["+user+"] repository["+repository+"] issueId["+issueId+"] will change to ["+newStatus+"]");
+    headers = { 'Authorization': authorization, 'User-Agent': 'request' };
+    requestBody = JSON.stringify({ "state": newStatus });
+    console.log("Tim - user[" + user + "] repository[" + repository + "] issueId[" + issueId + "] will change to [" + newStatus + "]");
 
     // Update issuse using GitHub API
-    var url = 'https://api.github.com/repos/'+ user + '/' + repository + '/issues/' + issueId;
-    request.patch({'url':url, 'body':requestBody, 'headers':headers}, function(err, res, resBody){
-        if (err) {
-            return hook.res.end(err.messsage);
-        }
+  //  var url = 'https://api.github.com/repos/' + user + '/' + repository + '/issues/' + issueId;
+    //request.patch({ 'url': url, 'body': requestBody, 'headers': headers }, function (err, res, resBody) {
+    //    if (err) {
+    //        return hook.res.end(err.messsage);
+    //    }
 
-        // Build response
-        hook.res.setHeader("Content-Type", "application/json");
-        hook.res.end(JSON.stringify({
-            'externalTaskExecutionStatus' : 'FINISHED',
-            'executionContext' : {},
-            'taskState' : "Issue #"+issueId+" is "+newStatus,
-            'detailedInfo': "Issue number "+issueId+" state is now "+newStatus,
-            'progress' : 100,
-            'delayTillNextPoll' : 0
-        }));
-    })
+    //    // Build response
+    //    hook.res.setHeader("Content-Type", "application/json");
+    //    hook.res.end(JSON.stringify({
+    //        'externalTaskExecutionStatus': 'FINISHED',
+    //        'executionContext': {},
+    //        'taskState': "Issue #" + issueId + " is " + newStatus,
+    //        'detailedInfo': "Issue number " + issueId + " state is now " + newStatus,
+    //        'progress': 100,
+    //        'delayTillNextPoll': 0
+    //    }));
+    //})
 };
